95d9ad5ed1c3b605e98a48c919e967f86638c0ee
devansh0331 <devanshshrivastava07@gmail.com>
2024-05-11 19:59:59 +0530

    bugs sort-out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,18 @@ function Navbar() {
     e.preventDefault();
     navigate("/signin");
   };
+
+  const handleLogoNavigate = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
   return (
     <nav className="fixed py-5 top-0 flex justify-center w-screen overflow-hidden">
       <div className="w-11/12 flex items-center justify-between">
-        <div>Logo</div>
+        <div onClick={handleLogoNavigate} className="cursor-pointer">
+          Logo
+        </div>
         <div className="flex items-center justify-end">
           <Button
             onClick={handleNavigate}
