a504e78c1fa6b6db8f2ce1ddc9b9c76d024a800d
devansh0331 <devanshshrivastava07@gmail.com>
2024-05-17 04:05:16 +0530

    sidebar added

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import profile from "../assets/profile3.png";
+import { FaEdit } from "react-icons/fa";
+
 function SideBar() {
   return (
-    <div className="w-full mb-4 md:mb:0 shadow-none md:shadow-md py-3 px-6">
-      <div className="flex flex-col items-center justify-evenly py-3">
+    <div className="relative w-full mb-4 md:mb:0 shadow-none md:shadow-md py-3 px-6">
+      <div className="relative flex flex-col items-center justify-evenly py-3">
         <img
           src={profile}
           alt=""
@@ -18,7 +20,7 @@ function SideBar() {
         </p>
       </div>
       <hr />
-      <div className="text-sm  text-center  py-3">
+      <div className=" text-sm  text-center  py-3">
         <p className="font-semibold">
           Connections: <span className="font-normal">500+</span>{" "}
         </p>
@@ -28,6 +30,9 @@ function SideBar() {
         <p className="font-semibold">
           Impressions: <span className="font-normal">587k</span>
         </p>
+      </div>
+      <div className="absolute right-2 top-2">
+        <FaEdit className="text-2xl md:text-xl" />
       </div>
     </div>
   );
