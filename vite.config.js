5bc5c68ad6c21683e3da42a0b7f2e3075b51832e
devansh0331 <devanshshrivastava07@gmail.com>
2024-09-01 23:42:30 +0530

    dynamic routing config reversed

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: ".next",
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name][extname]",
-      },
-    },
-  },
 });
