5523259674ea5e4dec33b33c21bf111bc8c846fa
Yukti Jhawar <yuktimj2002@gmail.com>
2024-10-01 16:49:13 +0530

    view Applications responsive

--- src/components/JobCardSingle.jsx
+++ src/components/JobCardSingle.jsx
@@ -19,36 +19,52 @@ import React from "react";
 const JobCardSingle = (props) => {
   const job = props.job;
   // console.log(job);
-  
-  return (
-    <div className="bg-white w-full p-4 max-h-[80vh] flex flex-col justify-start overflow-y-scroll scrollbar-invisible mb-4 rounded-xl">
 
+  return (
+    <div className={`bg-white w-full p-4 max-h-[80vh] flex flex-col justify-start mb-4 rounded-xl ${props.route === "ViewApplications" ? "md:overflow-y-scroll scrollbar-invisible overflow-visible" : "overflow-y-scroll scrollbar-invisible"}`}>
       <div
-        className={`p-2 m-0 flex justify-between gap-2 ${props.route === "ViewApplications" ? 'flex-col' : " flex-col sm:flex-row lg:flex-col 2xl:flex-row"}`}
+        className={`p-2 m-0 flex justify-between gap-2 ${
+          props.route === "ViewApplications"
+            ? "flex-col"
+            : " flex-col sm:flex-row lg:flex-col 2xl:flex-row"
+        }`}
       >
         <div className="flex flex-col">
           <Typography className="text-2xl tracking-wide text-black w-full font-semibold">
-            {job?.jobTitle ? job.jobTitle : "N/A"}, {job?.organziationName ? job?.organziationName : ""}
+            {job?.jobTitle ? job.jobTitle : "N/A"},{" "}
+            {job?.organziationName ? job?.organziationName : ""}
           </Typography>
           <Typography className="flex text-lg text-gray-800">
-            <span className="">{job?.location ? job.location : job?.organizationName}</span>
+            <span className="">
+              {job?.location ? job.location : job?.organizationName}
+            </span>
           </Typography>
         </div>
         <div className="flex flex-col">
           <Typography className="text-sm">250 Applicants</Typography>
           <Typography className="text-sm">
             Posted by,{" "}
-            <span className="text-blue-500 cursor-pointer">{job?.user ? job.user : "N/A"}</span>
+            <span className="text-blue-500 cursor-pointer">
+              {job?.user ? job.user : "N/A"}
+            </span>
           </Typography>
         </div>
       </div>
 
       <CardBody className="m-0 p-0 pb-4 border-b-2">
         {/* DETAILS 1 */}
-        <div className={`flex ${props.route === "ViewApplications" ? 'flex-col gap-1' : "flex-col sm:flex-row lg:flex-col 2xl:flex-row gap-2 sm:gap-5 lg:gap-2 2xl:gap-5"} text-gray-700 w-full px-5 mt-3 `}>
+        <div
+          className={`flex ${
+            props.route === "ViewApplications"
+              ? "flex-col gap-1"
+              : "flex-col sm:flex-row lg:flex-col 2xl:flex-row gap-2 sm:gap-5 lg:gap-2 2xl:gap-5"
+          } text-gray-700 w-full px-5 mt-3 `}
+        >
           <Typography className="flex items-center gap-1">
             <TbPigMoney className="w-5 h-5 text-black" />
-            <span className="">{job?.salaryRange ? job.salaryRange : "N/A"}</span>
+            <span className="">
+              {job?.salaryRange ? job.salaryRange : "N/A"}
+            </span>
           </Typography>
           <Typography className="flex items-center gap-1">
             <TiDocumentText className="w-5 h-5 text-black" />
@@ -60,7 +76,9 @@ const JobCardSingle = (props) => {
           </Typography>
           <Typography className="flex items-center gap-1">
             <FaUserGraduate className="w-5 h-5 text-black" />
-            <span className="">{job?.requiredQualification ? job.requiredQualification : "N/A"}</span>
+            <span className="">
+              {job?.requiredQualification ? job.requiredQualification : "N/A"}
+            </span>
           </Typography>
           <Typography className="flex items-center gap-1">
             <HiHomeModern className="w-5 h-5 text-black" />
@@ -78,44 +96,47 @@ const JobCardSingle = (props) => {
         {/* APPLICATION */}
         {props.route === "ViewApplications" ? (
           <></>
-        ):(
-        <div className="flex text-gray-700 w-full px-3 mt-6 gap-5 items-end">
-          <Button className="" size="sm" color="blue">
-            Apply
-          </Button>
-          <Button className="" size="sm" color="blue" variant="outlined">
-            Save
-          </Button>
-        </div>
+        ) : (
+          <div className="flex text-gray-700 w-full px-3 mt-6 gap-5 items-end">
+            <Button className="" size="sm" color="blue">
+              Apply
+            </Button>
+            <Button className="" size="sm" color="blue" variant="outlined">
+              Save
+            </Button>
+          </div>
         )}
       </CardBody>
 
       <CardBody className="m-0 p-0 mt-3">
         <div className="w-full py-2 px-3">
-        <Typography className="text-lg text-gray-700 font-semibold">Job Description</Typography>
-        <p className="flex flex-col text-sm list-disc mx-5 mt-1" >
-          {job?.jobDescription ? job.jobDescription : "N/A"}
-        </p>
-        <Typography className="text-lg text-gray-700 font-semibold mt-2">Benefits</Typography>
-        <p className="flex flex-col text-sm list-disc mx-5 mt-1" >
-          {job?.benefits ? job.benefits : "N/A"}
-        </p>
+          <Typography className="text-lg text-gray-700 font-semibold">
+            Job Description
+          </Typography>
+          <p className="flex flex-col text-sm list-disc mx-5 mt-1">
+            {job?.jobDescription ? job.jobDescription : "N/A"}
+          </p>
+          <Typography className="text-lg text-gray-700 font-semibold mt-2">
+            Benefits
+          </Typography>
+          <p className="flex flex-col text-sm list-disc mx-5 mt-1">
+            {job?.benefits ? job.benefits : "N/A"}
+          </p>
         </div>
 
         {props.route === "ViewApplications" ? (
           <></>
-        ):(
-        <div className="flex text-gray-700 w-full px-3 mt-6 gap-5 items-end">
-          <Button className="" size="sm" color="blue">
-            Apply
-          </Button>
-          <Button className="" size="sm" color="blue" variant="outlined">
-            Save
-          </Button>
-        </div>
+        ) : (
+          <div className="flex text-gray-700 w-full px-3 mt-6 gap-5 items-end">
+            <Button className="" size="sm" color="blue">
+              Apply
+            </Button>
+            <Button className="" size="sm" color="blue" variant="outlined">
+              Save
+            </Button>
+          </div>
         )}
       </CardBody>
-
     </div>
   );
 };
--- src/pages/ViewApplications.jsx
+++ src/pages/ViewApplications.jsx
@@ -1,114 +1,174 @@
-import React, { useContext, useEffect, useState } from 'react'
-import SideBar from '../components/SideBar'
-import JobCardSingle from '../components/JobCardSingle'
-import { UserContext } from '../UserContext'
-import { Avatar, Badge, Button, Card, CardBody, CardFooter, CardHeader, Typography } from '@material-tailwind/react'
+import React, { useContext, useEffect, useState } from "react";
+import SideBar from "../components/SideBar";
+import JobCardSingle from "../components/JobCardSingle";
+import { UserContext } from "../UserContext";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Dialog,
+  Typography,
+} from "@material-tailwind/react";
 import altprofile from "../assets/altprofile.png";
 import { IoLocationSharp } from "react-icons/io5";
 import { BsBuildingsFill } from "react-icons/bs";
 import { IoMdSearch } from "react-icons/io";
 import YouMayKnow from "../components/YouMayKnow";
 import ProfileExpand from "../components/ProfileExpand";
-import { SERVER_URL } from '../ServerURL'
+import { SERVER_URL } from "../ServerURL";
+import { FaChevronDown } from "react-icons/fa";
 
 const ViewApplications = () => {
-  const [jobs,setJobs] = useState([])
+  const [jobs, setJobs] = useState([]);
   const [userExpand, setUserExpand] = useState({});
-  const {allUsers, getAllUsers, userInfo} = useContext(UserContext);
+  const { allUsers, getAllUsers, userInfo } = useContext(UserContext);
+  const [jobOpen, setJobOpen] = useState(false);
+  const [profileExpand, setProfileExpand] = useState(false);
 
-  const getAllJobs = async() => {
+  const handleProfileExpand = () => {
+    setProfileExpand(!profileExpand);
+  };
+  const getAllJobs = async () => {
     try {
-      const response = await fetch(`${SERVER_URL}/job/all-jobs`,{
+      const response = await fetch(`${SERVER_URL}/job/all-jobs`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-      })
+      });
       const res = await response.json();
       // console.log(res)
-      if(res.success){
+      if (res.success) {
         setJobs(res.jobs);
       }
-    }
-     catch (error) {
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getAllJobs();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    getAllUsers(); 
+    setUserExpand(allUsers[0]);
+  }, [allUsers]);
+
+  useEffect(() => {
+    getAllUsers();
   }, [userInfo]);
 
   return (
-    <div className='flex overflow-y-hidden z-0 bg-background h-[90vh]'>
-        <SideBar />
-        <div className='flex w-[95%] 2xl:w-[90%] mx-auto h-full gap-6 justify-center mt-5'>
-          <div className="w-1/3">
-            <JobCardSingle job={jobs[0]} route={"ViewApplications"} />
-          </div>
-            <div className="grid md:grid-cols-2 overflow-y-scroll max-h-[80vh] scrollbar-thin w-1/2 gap-3">
-            {allUsers.map((user, index) => ( 
-             <>
-             {user.name != "" ? (    
-            <Card className="p-3 flex flex-col gap-3 justify-between" key={index}>
-              <CardHeader
-                floated={false}
-                shadow={false}
-                color="transparent"
-                className="p-0 m-0  flex flex-col items-center justify-center w-full border-b-2 rounded-none pb-2 "
-              >
-                <Badge color="green" overlap="circular" invisible>
-                  <Avatar
-                    src={user.profileURL ? user.profileURL : altprofile}
-                    alt="altprofile"
-                    size="xl"
-                    className="w-24 h-24 mx-auto"
-                  />
-                </Badge>
-                <Typography className="text-lg mt-2">{user.name}</Typography>
-              </CardHeader>
-              {user.location || user.bio ? (  
-              <CardBody className="m-0 p-0 border-b-2 rounded-none pb-2">
-                {user.bio && (
-
-                  <Typography className="flex items-center">
-                  <BsBuildingsFill />
-                  <span className="ml-1">{user.bio}</span>
-                </Typography>
-                )}
-                {user.location && (
-                  <Typography className="flex items-center">
-                  <IoLocationSharp />
-                  <span className="ml-1">{user.location}</span>
-                </Typography>
-                )}          
-              </CardBody>
-              ): null}
-              <CardFooter className="m-0 p-0 mx-auto">
-                <Button
-                  size="sm"
-                  className="px-2 py-1 font-light rounded-md"
-                  color="light-blue"
-                  onClick={() => setUserExpand(user)}
-                >
-                  Show Details
-                </Button>
-              </CardFooter>
-            </Card>
-              ) : null}
-             </>
+    <div className="flex md:overflow-y-hidden z-0 bg-background h-[90vh] w-full overflow-y-scroll scrollbar-thin">
+      <SideBar />
+      <div className="flex w-[92%] lg:w-[90%] 2xl:w-[80%] mx-auto h-full gap-6 justify-center mt-5">
+        <div className="lg:w-1/3 w-2/5 hidden md:block">
+          <JobCardSingle job={jobs[0]} route={"ViewApplications"} />
+        </div>
+        <div className="flex flex-col w-full lg:w-1/2 md:w-3/5">
+          <Accordion
+            onClick={() => setJobOpen(!jobOpen)}
+            open={jobOpen}
+            className="bg-white mb-2 rounded-lg px-4  md:hidden"
+            icon={<FaChevronDown />}
+          >
+            <AccordionHeader className="border-none">
+              {jobs[0]?.jobTitle}, {jobs[0]?.organziationName}
+            </AccordionHeader>
+            <AccordionBody>
+              <JobCardSingle job={jobs[0]} route={"ViewApplications"} />
+            </AccordionBody>
+          </Accordion>
+          <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-2 overflow-visible md:overflow-y-scroll max-h-[80vh] scrollbar-thin gap-3">
+            {allUsers.map((user, index) => (
+              <>
+                {user.name != "" ? (
+                  <Card
+                    className="p-3 flex flex-col gap-3 justify-between"
+                    key={index}
+                  >
+                    <CardHeader
+                      floated={false}
+                      shadow={false}
+                      color="transparent"
+                      className="p-0 m-0  flex flex-col items-center justify-center w-full border-b-2 rounded-none pb-2 "
+                    >
+                      <Badge color="green" overlap="circular" invisible>
+                        <Avatar
+                          src={user.profileURL ? user.profileURL : altprofile}
+                          alt="altprofile"
+                          size="xl"
+                          className="w-24 h-24 mx-auto"
+                        />
+                      </Badge>
+                      <Typography className="text-lg mt-2">
+                        {user.name}
+                      </Typography>
+                    </CardHeader>
+                    {user.location || user.bio ? (
+                      <CardBody className="m-0 p-0 border-b-2 rounded-none pb-2">
+                        {user.bio && (
+                          <Typography className="flex items-center">
+                            <BsBuildingsFill />
+                            <span className="ml-1">{user.bio}</span>
+                          </Typography>
+                        )}
+                        {user.location && (
+                          <Typography className="flex items-center">
+                            <IoLocationSharp />
+                            <span className="ml-1">{user.location}</span>
+                          </Typography>
+                        )}
+                      </CardBody>
+                    ) : null}
+                    <CardFooter className="m-0 p-0 mx-auto">
+                      <Button
+                        size="sm"
+                        className="px-2 py-1 font-light rounded-md hidden xl:block"
+                        color="light-blue"
+                        onClick={() => setUserExpand(user)}
+                      >
+                        Show Details
+                      </Button>
+                      <Button
+                        size="sm"
+                        className="px-2 py-1 font-light rounded-md xl:hidden block"
+                        color="light-blue"
+                        onClick={() => {
+                          setUserExpand(user);
+                          handleProfileExpand();
+                        }}
+                      >
+                        Show Details
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                ) : null}
+              </>
             ))}
           </div>
-          <div className="w-1/4">
-          <ProfileExpand user={userExpand}/>
-          </div>
         </div>
+        <div className="w-1/4 hidden xl:block">
+          <ProfileExpand user={userExpand} />
+        </div>
+      </div>
+      <Dialog
+        open={profileExpand}
+        handler={handleProfileExpand}
+        className="bg-transparent shadow-none"
+        size="xs"
+      >
+        <ProfileExpand user={userExpand} />
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default ViewApplications
+export default ViewApplications;
